b80df14805e4f9068d095134ee04832bf556e53b
Paige Miles <paige@lucas-miles.com>
2020-09-03 01:14:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,9 +6,8 @@ def create_an_array
   ["red", "yellow", "green", "blue"]
 end
 
-def add_element_to_end_of_array(array, element) {
+def add_element_to_end_of_array(array, element)
  array.push("element")
-}
 end
 
 def add_element_to_start_of_array(array, element)
